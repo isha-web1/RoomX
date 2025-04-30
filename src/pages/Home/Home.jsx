6fd5bd9f13b0a968dd505618e138f2ba156e7c1a
isha-web1 <isha.web1@gmail.com>
2025-04-30 20:45:16 +0600

    home page,login,signUp,and many components added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import Rooms from "../../components/Home/Rooms";
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      {/* Categories section  */}
+      <Categories />
+      {/* Rooms section */}
+      <Rooms />
+    </div>
+  );
 };
 
 export default Home;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,17 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage";
+import RoomDetails from "../pages/RoomDetails/RoomDetails";
+import Login from "../pages/Login/Login";
+import SignUp from "../pages/SignUp/SignUp";
 
 export const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Main/>,
-        children : [
-            {
-                path  : '/',
-                element : <Home/>
-            }
-        ]
-
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/room/:id",
+        element: <RoomDetails />,
+      },
+    ],
+  },
+  { path: '/login', element: <Login /> },
+  { path: '/signup', element: <SignUp /> },
+]);
