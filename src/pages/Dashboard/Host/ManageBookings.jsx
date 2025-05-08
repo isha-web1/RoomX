01cf774c1f5337429ea161ee1efd94e40f3d33f5
isha-web1 <isha.web1@gmail.com>
2025-05-08 23:32:02 +0600

    manage my bookings & mangae booking as host

--- src/pages/Dashboard/Host/ManageBookings.jsx
+++ src/pages/Dashboard/Host/ManageBookings.jsx
@@ -1,10 +1,30 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import LoadingSpinner from "../../../components/shared/LoadingSpinner";
+import BookingDataRow from "../../../components/TableRows/BookingDataRow";
 
 const ManageBookings = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  //   Fetch Bookings Data
+  const {
+    data: bookings = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["my-bookings", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/manage-bookings/${user?.email}`);
+
+      return data;
+    },
+  });
+
+  console.log(bookings);
+  if (isLoading) return <LoadingSpinner />;
   return (
     <>
-     
-
       <div className="container mx-auto px-4 sm:px-8">
         <div className="py-8">
           <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
@@ -50,7 +70,17 @@ const ManageBookings = () => {
                     </th>
                   </tr>
                 </thead>
-                <tbody>{/* Table row data */}</tbody>
+                <tbody>
+                  {" "}
+                  {/* Table Row Data */}
+                  {bookings.map((booking) => (
+                    <BookingDataRow
+                      key={booking._id}
+                      booking={booking}
+                      refetch={refetch}
+                    />
+                  ))}
+                </tbody>
               </table>
             </div>
           </div>
