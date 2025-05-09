9ca0a57919a7febc9c5148dc92d76141efb1c732
isha-web1 <isha.web1@gmail.com>
2025-05-09 23:30:02 +0600

    manage booking rooms & update roomData,add form

--- src/components/TableRows/RoomDataRows.jsx
+++ src/components/TableRows/RoomDataRows.jsx
@@ -1,64 +1,65 @@
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import {
-    Description,
-    Dialog,
-    DialogPanel,
-    DialogTitle,
-  } from '@headlessui/react'
-import { useState } from 'react'
-import DeleteModal from '../Modal/DeleteModal'
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { useState } from "react";
+import DeleteModal from "../Modal/DeleteModal";
+import UpdateRoomModal from "../Modal/UpdateRoomModal";
 
 const RoomDataRow = ({ room, refetch, handleDelete }) => {
-     // for delete modal
-  let [isOpen, setIsOpen] = useState(false)
-
+  // for delete modal
+  const [isOpen, setIsOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const closeModal = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
   return (
     <tr>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <div className='flex items-center'>
-          <div className='flex-shrink-0'>
-            <div className='block relative'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <div className="flex items-center">
+          <div className="flex-shrink-0">
+            <div className="block relative">
               <img
-                alt='profile'
+                alt="profile"
                 src={room?.image}
-                className='mx-auto object-cover rounded h-10 w-15 '
+                className="mx-auto object-cover rounded h-10 w-15 "
               />
             </div>
           </div>
-          <div className='ml-3'>
-            <p className='text-gray-900 whitespace-no-wrap'>{room?.title}</p>
+          <div className="ml-3">
+            <p className="text-gray-900 whitespace-no-wrap">{room?.title}</p>
           </div>
         </div>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>{room?.location}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">{room?.location}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>${room?.price}</p>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">${room?.price}</p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>
-          {format(new Date(room?.from), 'P')}
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">
+          {format(new Date(room?.from), "P")}
         </p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <p className='text-gray-900 whitespace-no-wrap'>
-          {format(new Date(room?.to), 'P')}
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <p className="text-gray-900 whitespace-no-wrap">
+          {format(new Date(room?.to), "P")}
         </p>
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-      <button
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <button
           onClick={() => setIsOpen(true)}
-          className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
         >
           <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-red-200 opacity-50 rounded-full'
+            aria-hidden="true"
+            className="absolute inset-0 bg-red-200 opacity-50 rounded-full"
           ></span>
-          <span className='relative'>Delete</span>
+          <span className="relative">Delete</span>
         </button>
         {/* Delete modal */}
         <DeleteModal
@@ -67,23 +68,28 @@ const RoomDataRow = ({ room, refetch, handleDelete }) => {
           handleDelete={handleDelete}
           id={room?._id}
         />
-        
-        
       </td>
-      <td className='px-5 py-5 border-b border-gray-200 bg-white text-sm'>
-        <span className='relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight'>
+      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+        <button
+          onClick={() => setIsEditModalOpen(true)}
+          className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight"
+        >
           <span
-            aria-hidden='true'
-            className='absolute inset-0 bg-green-200 opacity-50 rounded-full'
+            aria-hidden="true"
+            className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
           ></span>
-          <span className='relative'>Update</span>
-        </span>
+          <span className="relative">Update</span>
+        </button>
         {/* Update Modal */}
+        <UpdateRoomModal
+          isOpen={isEditModalOpen}
+          setIsEditModalOpen={setIsEditModalOpen}
+          room={room}
+          refetch={refetch}
+        />
       </td>
     </tr>
-  )
-}
+  );
+};
 
-
-
-export default RoomDataRow
+export default RoomDataRow;
